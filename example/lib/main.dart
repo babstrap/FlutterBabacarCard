b9b59464031fdccaa0fea285aff1070e3611ecfd
Babacar <ndongbabacar100@gmail.com>
2021-06-26 17:54:34 +0000

    Change the Example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,56 +1,34 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:flutter_babacar_card/rounded_card.dart';
 
-import 'package:flutter/services.dart';
-import 'package:flutter_babacar_card/flutter_babacar_card.dart';
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterBabacarCard.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+      debugShowCheckedModeBanner: false,
+      title: 'Learning to build Package',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: RoundedCard(
+        cardColor: Colors.red,
+        cardRaduis: 20.0,
+        cardIcon: Icon(
+          Icons.ac_unit_rounded,
+          size: 100,
+          color: Colors.white,
         ),
       ),
     );
